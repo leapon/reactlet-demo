4418da40c1575d62c5768afcf36e2688190008d8
leapon <uestudio@gmail.com>
2015-04-04 21:40:47 -0400

    fixed build problem

---
+++ site/public/script/reactlet/src/treeview.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var bootstrap = require('bootstrap');
+var treeview = require('reactlet-treeview');
+
+var app = app || {};
+
+$().ready(function() {
+    console.log('in reactlet treeview page');
+});
