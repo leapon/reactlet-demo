9bbf99cd5575df4d8efac9078a85e1f3ee6c17a4
leapon <uestudio@gmail.com>
2015-04-04 21:57:08 -0400

    treeview display works

--- site/public/script/reactlet/src/treeview.js
+++ site/public/script/reactlet/src/treeview.js
@@ -2,10 +2,51 @@
 
 var React = require('react');
 var bootstrap = require('bootstrap');
-var treeview = require('reactlet-treeview');
+var Treeview = require('reactlet-treeview');
 
 var app = app || {};
 
+app.tree1Data = {
+    expandLevel: 3,
+    treedata: [
+        {
+            "name": "website",
+            "iconClass": "fa fa-folder-o",
+            "children": [
+                {
+                    "name": "images",
+                    "iconClass": "fa fa-folder-o",
+                    "children": [
+                        {
+                            "name": "logo.png",
+                            "iconClass": "fa fa-file-image-o"
+                        },
+                        {
+                            "name": "background.png",
+                            "iconClass": "fa fa-file-image-o"
+                        }
+                    ]
+                },
+                {
+                    "name": "index.html",
+                    "iconClass": "fa fa-file-text-o"
+                },
+                {
+                    "name": "about.html",
+                    "iconClass": "fa fa-file-text-o"
+                },
+                {
+                    "name": "product.html",
+                    "iconClass": "fa fa-file-text-o"
+                }
+            ]
+        }
+    ]
+};
+
 $().ready(function() {
-    console.log('in reactlet treeview page');
+    app.treeview1 = React.renderComponent(
+        <Treeview data={ app.tree1Data } />,
+        document.getElementById('treeview1')
+    );
 });
