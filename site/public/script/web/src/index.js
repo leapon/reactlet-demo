d409e6784eaf7b7a9731789414d37dc79b758e4e
leapon <uestudio@gmail.com>
2015-04-04 20:53:16 -0400

    update script

---
+++ site/public/script/web/src/index.js
@@ -0,0 +1,10 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var bootstrap = require('bootstrap');
+
+var app = app || {};
+
+$().ready(function() {
+    console.log('in web page');
+});
